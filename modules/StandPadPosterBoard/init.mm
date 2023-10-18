25d9aec0c0d26ba443d5685bdc8ad0275d7bfc11
Jinwoo Kim <kidjinwoo@me.com>
2023-10-19 03:22:51 +0900

    no message

--- modules/StandPadPosterBoard/init.mm
+++ modules/StandPadPosterBoard/init.mm
@@ -23,45 +23,10 @@ namespace SP_os_feature_enabled_impl {
     }
 }
 
-namespace SP_PRPosterAmbientConfiguration {
-    namespace setHidden {
-        void (*original)(id self, SEL _cmd, BOOL arg0);
-        void custom(id self, SEL _cmd, BOOL arg0) {
-            original(self, _cmd, NO);
-        }
-
-        void hook() {
-            MSHookMessageEx(
-                NSClassFromString(@"PRPosterAmbientConfiguration"),
-                NSSelectorFromString(@"setHidden:"),
-                reinterpret_cast<IMP>(&custom),
-                reinterpret_cast<IMP *>(&original)
-            );
-        }
-    }
-
-    namespace hidden {
-        BOOL custom(id self, SEL _cmd) {
-            return NO;
-        }
-
-        void hook() {
-            MSHookMessageEx(
-                NSClassFromString(@"PRPosterAmbientConfiguration"),
-                NSSelectorFromString(@"hidden"),
-                reinterpret_cast<IMP>(&custom),
-                nullptr
-            );
-        }
-    }
-}
-
 __attribute__((constructor)) static void init() {
     NSAutoreleasePool *pool = [NSAutoreleasePool new];
 
     SP_os_feature_enabled_impl::hook();
-    SP_PRPosterAmbientConfiguration::setHidden::hook();
-    SP_PRPosterAmbientConfiguration::hidden::hook();
 
     [pool release];
 }
